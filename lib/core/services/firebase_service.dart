d1fc94a5172ce90152cfd03e6f943208f061adc7
dizdaroglu <furkndizdaroglu@gmail.com>
2020-05-11 04:05:15 +0300

    login view oluşturuldu

--- lib/core/services/firebase_service.dart
+++ lib/core/services/firebase_service.dart
@@ -4,12 +4,31 @@ import 'dart:io';
 
 import 'package:flutter_fire/model/students.dart';
 import 'package:flutter_fire/model/user.dart';
+import 'package:flutter_fire/model/user/user_auth_error.dart';
+import 'package:flutter_fire/model/user/user_request.dart';
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
 import 'package:flutter_fire/apiURL.dart';
 
 class FirebaseService {
-  static const String FIREBASE_URL = api;
+  static const String FIREBASE_URL = firebase_url;
+  static const String FIREBASE_AUTH_URL = firebase_auth_url;
+
+  Future postUser(UserRequest request) async {
+    var jsonModel = json.encode(request.toJson());
+
+    final response = await http.post(FIREBASE_AUTH_URL, body: jsonModel);
+
+    switch (response.statusCode) {
+      case HttpStatus.ok:
+        return true;
+
+      default:
+        var errorJson = json.decode(response.body);
+        var error = FirebaseAuthError.fromJson(errorJson);
+        return error;
+    }
+  }
 
   Future<List<User>> getUsers() async {
     final response = await http.get("$FIREBASE_URL/users.json");
--- lib/ui/views/authentication/login_view.dart
+++ lib/ui/views/authentication/login_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fire/core/services/firebase_service.dart';
+import 'package:flutter_fire/model/user/user_auth_error.dart';
+import 'package:flutter_fire/model/user/user_request.dart';
 import 'package:flutter_fire/ui/shared/styles/login_view_styles.dart';
 import 'package:flutter_fire/ui/shared/widgets/rounded_button.dart';
+import 'package:flutter_fire/ui/views/home/home_view.dart';
 
 class LoginView extends StatefulWidget {
   @override
@@ -8,9 +12,15 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  String username;
+  String password;
+  FirebaseService service = FirebaseService();
+  GlobalKey<ScaffoldState> scaffold = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffold,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
@@ -29,6 +39,11 @@ class _LoginViewState extends State<LoginView> {
                     ),
                     TextField(
                       decoration: inputDecorationStyle,
+                      onChanged: (val) {
+                        setState(() {
+                          username = val;
+                        });
+                      },
                     ),
                     SizedBox(
                       height: 12,
@@ -36,12 +51,32 @@ class _LoginViewState extends State<LoginView> {
                     TextField(
                       decoration:
                           inputDecorationStyle.copyWith(labelText: "Password"),
+                      obscureText: true,
+                      onChanged: (val) {
+                        setState(() {
+                          password = val;
+                        });
+                      },
                     ),
                     SizedBox(
                       height: 24,
                     ),
                     RoundedButton(
-                      onPressed: () {},
+                      onPressed: () async {
+                        var result = await service.postUser(UserRequest(
+                            email: username,
+                            password: password,
+                            returnSecureToken: true));
+                        if (result is FirebaseAuthError) {
+                          scaffold.currentState.showSnackBar(
+                              SnackBar(content: Text(result.error.message)));
+                        } else {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomeView()));
+                        }
+                      },
                       colour: Color(0xffe74c3c),
                       title: "Login",
                     )
